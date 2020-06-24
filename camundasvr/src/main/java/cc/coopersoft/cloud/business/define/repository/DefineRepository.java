40441ae2a9b284a84bf3191e2c5270341acbd015
cooper <lyt3131@163.com>
2020-06-25 00:27:55 +0800

    move camunda to business

--- camundasvr/src/main/java/cc/coopersoft/cloud/business/define/repository/DefineRepository.java
+++
@@ -1,9 +0,0 @@
-package cc.coopersoft.cloud.business.define.repository;
-
-import cc.coopersoft.cloud.business.define.model.BusinessDefine;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DefineRepository extends CrudRepository<BusinessDefine,String> {
-}
