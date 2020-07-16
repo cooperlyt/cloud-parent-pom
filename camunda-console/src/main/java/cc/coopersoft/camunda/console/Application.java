fedbf4bcc2ed48f2fc6f4fcc2ee1e5fed5e808b1
cooper <lyt3131@163.com>
2020-07-16 11:53:15 +0800

    迁移项目到 spring cloud 5.2.X , 项目仅保留 gateway 其它均作废

--- camunda-console/src/main/java/cc/coopersoft/camunda/console/Application.java
+++ camunda-console/src/main/java/cc/coopersoft/camunda/console/Application.java
@@ -3,9 +3,10 @@ package cc.coopersoft.camunda.console;
 import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
-@SpringBootConfiguration
+@SpringBootApplication
 @EnableProcessApplication
 public class Application {
     public static void main(String... args){
